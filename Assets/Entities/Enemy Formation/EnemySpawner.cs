c0d1f28ac76dd5a11339f976efa6052eb51bcf9a
Daniel Wilson <wilsondr16@gmail.com>
2015-12-27 15:18:43 -0600

    Added player and enemies. Added enemy formation

---
+++ Assets/Entities/Enemy Formation/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in this.transform){
+			//spawn a new enemy at a position within the formation
+			GameObject enemy = GameObject.Instantiate(enemyPrefab,child.transform.position,Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float padding = 1.0f;
+	
+	float xMin;
+	float xMax;
+	// Use this for initialization
+	void Start () {
+		float zDistance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBound = Camera.main.ViewportToWorldPoint(new Vector3(0,0,zDistance));
+		Vector3 rightBound = Camera.main.ViewportToWorldPoint(new Vector3(1,0,zDistance));
+		xMin = leftBound.x + padding;
+		xMax = rightBound.x - padding;
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKey(KeyCode.RightArrow)){
+			this.transform.position += new Vector3(speed * Time.deltaTime,0f, 0f);
+		
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			this.transform.position += new Vector3(-speed * Time.deltaTime,0f, 0f);
+		}
+		
+		float clampedX = Mathf.Clamp(transform.position.x,xMin,xMax);
+		transform.position = new Vector3(clampedX,transform.position.y, 0f);
+	}
+}
